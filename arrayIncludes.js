cabfa327beb2c600cb609d35f92f53c6795c6eaa
nitya2508 <nityashree2508@gmail.com>
2021-12-29 12:31:02 +0530

    few examples added

--- arrayIncludes.js
+++ arrayIncludes.js
@@ -5,3 +5,18 @@ if (marks.includes(50)){
 }else{
     console.log('could not find element')
 }
+
+
+if (marks.includes(60,2)){//1st param is element to search, 2nd param is index from where search begin
+    console.log('found element')
+}else{
+    console.log('element not found')
+}
+
+let user1 = {name:'apple'},
+user2 = {name:'mango'},
+user3={name:'banana'}
+let users = [user1,user2]
+
+console.log(users.includes(user1, 0))
+console.log(users.includes(user3))
